4f33bc9f02bf6e3d12fea4fe3d2924e10d5161e8
BRUNO GOMES DA SILVA <BRUNO6162@DIGITALCOLLEGE.LOCAL>
2023-09-13 10:10:06 -0300

    fix:lib

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,11 @@
 import 'react-native-url-polyfill/auto'
 import { createClient } from "@supabase/supabase-js";
-import { Database } from './database.types';
 
 const supabaseUrl = "https://fesbijsyucpghxhexzdf.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlc2JpanN5dWNwZ2h4aGV4emRmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NDUyNjYxMCwiZXhwIjoyMDEwMTAyNjEwfQ.IqXpFryR7Uk06h61vMjYeVDuB-VTkE7fxPjeaCi0-Go";
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth:{
     persistSession:false
   }
